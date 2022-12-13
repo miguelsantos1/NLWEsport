7f1f9738fbf937bf79f1e70f7d22df41815bc514
Miguel santos <miguel.santos6700@gmail.com>
2022-12-13 19:02:00 -0300

    Changed style in card hover

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,6 +1,6 @@
 export default function Game(props) {
   return(
-    <div className="hover:shadow-md hover:shadow-textColor transition duration-200">
+    <div className="shadow-md shadow-purple hover:shadow-lg hover:shadow-purple transition duration-200">
       <img className="rounded w-50 h-60" src={ props.image } alt="game banner" />
   </div>
   )
