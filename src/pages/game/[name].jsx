6f312e839c4ef8e702c6e81f6e39b705d3a98c73
Miguel santos <miguel.santos6700@gmail.com>
2022-12-13 14:06:54 -0300

    Added Loading component

--- src/pages/game/[name].jsx
+++ src/pages/game/[name].jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Link from 'next/link'
 
+import Loading from '../../components/Loading'
 import Ads from '../../components/Ads'
 
 export async function getServerSideProps(context) {
@@ -19,10 +20,12 @@ export async function getServerSideProps(context) {
 export default function Game(props) {
 
   const [game, setGame] = useState([])
+  const [removeLoading, setRemoveLoading] = useState(false)
 
   useEffect(() => {
     axios(`/api/games/${props.name}`).then((response => {
       setGame(response.data)
+      setRemoveLoading(true)
     }))
   }, [])
 
@@ -38,10 +41,13 @@ export default function Game(props) {
       <h2 className="text-textColor text-center font-extrabold my-5 text-xl sm:mb-16">  <span className="text-purple"> 	&lt; </span> { game.length } Anúncio (s)<span className="text-purple"> &gt; </span></h2>
     </header>
 
+
       <Link className="justify-center" href={`/post/${props.name}`}>
         <div className="text-center text-textColor rounded px-4 py-3 mb-9 font-bold hover:bg-purple/50 bg-purple/80"><h3> Fazer um anúncio </h3></div>
       </Link>
 
+
+          {!removeLoading && <Loading />}
       <section className="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3 mb-20">
 
           { game.map(ads => {
