8ef68ad9131e5b94f6987f2b4e4af7e08ecd80f7
Miguel santos <miguel.santos6700@gmail.com>
2022-12-13 23:22:29 -0300

    changed rounded in image

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,7 +1,7 @@
 export default function Game(props) {
   return(
     <div className="shadow-md shadow-purple hover:shadow-lg hover:shadow-purple transition duration-200">
-      <img className="rounded w-50 h-60" src={ props.image } alt="game banner" />
+      <img className="rounded-lg w-40 bg-cover" src={ props.image } alt="game banner" />
   </div>
   )
 }
