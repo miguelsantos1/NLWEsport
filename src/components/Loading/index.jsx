2e29259f6a232aa988a3d116e4c0d48270a9547f
Miguel santos <miguel.santos6700@gmail.com>
2022-12-13 23:52:50 -0300

    Added margin-top in Loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,5 +1,5 @@
 export default function Loading() {
   return(
-    <h1 className="text-textColor font-bold text-center text-2xl">Carregando anúncios...</h1>
+    <h1 className="text-textColor font-bold text-center mt-24 text-2xl">Carregando anúncios...</h1>
   )
 }
